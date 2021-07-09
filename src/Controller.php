eb225cc78b1123a10ce2784790f232d71c2066c4
Tam <hi@tam.sx>
2021-07-09 12:07:02 +0100

    Fix security vulnerability

--- src/Controller.php
+++ src/Controller.php
@@ -2,14 +2,20 @@
 
 namespace ether\logs;
 
+use Craft;
+
 class Controller extends \craft\web\Controller
 {
 
 	public function actionStream ()
 	{
-		$logsDir = \Craft::getAlias('@storage/logs');
-		$logFile = \Craft::$app->request->getParam('log');
-		$currentLog = \Craft::$app->request->get('log', $logFile);
+		$logsDir = Craft::getAlias('@storage/logs');
+		$logFile = Craft::$app->request->getParam('log');
+		$currentLog = basename(Craft::$app->request->get('log', $logFile));
+
+		if (strpos($currentLog, '.log') === false)
+			return '<p>You can only access <code>.log</code> files!</p>';
+
 		$log = file_get_contents($logsDir . '/' . $currentLog);
 
 		exit($log);

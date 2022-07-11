0ac69037e5d5a970ef715cb543bef08db1993b86
Tam <hi@tam.sx>
2022-07-11 13:33:24 +0100

    Craft 4

--- src/Service.php
+++ src/Service.php
@@ -11,15 +11,19 @@ class Service extends Component
 	public function truncate ($log)
 	{
 		$logsDir = Craft::getAlias('@storage/logs');
-		file_put_contents($logsDir . DIRECTORY_SEPARATOR . $log, '');
-		Craft::$app->getSession()->setNotice($log . ' truncated.');
+		$success = file_put_contents($logsDir . DIRECTORY_SEPARATOR . $log, '');
+
+		if ($success !== false) Craft::$app->getSession()->setNotice($log . ' truncated.');
+		else Craft::$app->getSession()->setError('Failed to truncate ' . $log . ', check file permissions');
 	}
 
 	public function delete ($log)
 	{
 		$logsDir = Craft::getAlias('@storage/logs');
-		unlink($logsDir . DIRECTORY_SEPARATOR . $log);
-		Craft::$app->getSession()->setNotice($log . ' deleted.');
+		$success = unlink($logsDir . DIRECTORY_SEPARATOR . $log);
+
+		if ($success !== false) Craft::$app->getSession()->setNotice($log . ' deleted.');
+		else Craft::$app->getSession()->setError('Failed to delete ' . $log . ', check file permissions');
 	}
 
 }
